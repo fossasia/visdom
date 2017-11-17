3aef713dc85a2d74a9a9c5d0896983c9dc784ba2
Jack Urbanek <jju@fb.com>
2017-11-17 07:50:39 -0800

    Bumping version number
    Summary: need to bump the version number for pip
    
    Reviewed By: lvdmaaten
    
    Differential Revision: D6348090
    
    fbshipit-source-id: 4a53307a3c5934927046d9f2cdb0e05b09f3217f

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requirements = [
 setup(
     # Metadata
     name='visdom',
-    version='0.1.6.2',
+    version='0.1.6.3',
     author='Allan Jabri',
     author_email='ajabri@fb.com',
     url='https://github.com/facebookresearch/visdom',
