fossasia/visdom

commit 1753e20f4ba805cb94d3646d472c53bc8d990a2a

remote API for lua
Summary:
allows for calling python visdom api from lua. the work ends up being done server-side.
this alleviates the overhead of implementing new plot types/features in lua.

note that torchfile will not be shipped to OSS (I just didn't want to bring it into tp2)

Reviewed By: lvdmaaten

Differential Revision: D5337983

fbshipit-source-id: de1d0db77b82383955a7236af0f313205efdc5c9

py/__init__.py
@@ -24,6 +24,10 @@ from six import string_types
 from six import BytesIO
 import logging
 
+try:
+    import torchfile
+except BaseException:
+    from . import torchfile
 
 logging.getLogger('requests').setLevel(logging.CRITICAL)
 logging.getLogger('urllib3').setLevel(logging.CRITICAL)
@@ -46,6 +50,19 @@ def nan2none(l):
         if math.isnan(val):
             l[idx] = None
     return l
+
+
+def from_t7(t, b64=False):
+    if b64:
+        t = base64.b64decode(t)
+
+    with open('/dev/shm/t7', 'w') as ff:
+        ff.write(t)
+        ff.close()
+
+    sf = open('/dev/shm/t7')
+
+    return torchfile.T7Reader(sf).read_obj()
 
 
 def loadfile(filename):
@@ -180,8 +197,7 @@ def _assert_opts(opts):
 
 def pytorch_wrap(fn):
     def result(*args, **kwargs):
-        args = (a.cpu().numpy() if type(a).__module__ == 'torch' else a
-                for a in args)
+        args = (a.cpu().numpy() if type(a).__module__ == 'torch' else a for a in args)
 
         for k in kwargs:
             if type(kwargs[k]).__module__ == 'torch':
@@ -209,6 +225,7 @@ class Visdom(object):
         ipv6=True,
         proxy=None,
         env='main',
+        send=True,
     ):
         self.server = server
         self.endpoint = endpoint
@@ -216,6 +233,7 @@ class Visdom(object):
         self.ipv6 = ipv6
         self.proxy = proxy
         self.env = env              # default env
+        self.send = send
 
         try:
             import torch
@@ -233,6 +251,9 @@ class Visdom(object):
         """
         if msg.get('eid', None) is None:
             msg['eid'] = self.env
+
+        if not self.send:
+            return msg, endpoint
 
         try:
             r = requests.post(
@@ -482,6 +503,10 @@ class Visdom(object):
         if name:
             assert len(name) >= 0, 'name of trace should be nonempty string'
             assert X.ndim == 1, 'updating by name expects 1-dim data'
+
+        if opts.get('markercolor') is not None:
+            opts['markercolor'] = _markerColorCheck(
+                opts['markercolor'], X, Y, K)
 
         data = {'x': X.transpose().tolist(), 'y': Y.transpose().tolist()}
         if X.ndim == 1:
@@ -844,7 +869,6 @@ class Visdom(object):
         - `opts.xmin`    : clip minimum value (`number`; default = `X:min()`)
         - `opts.xmax`    : clip maximum value (`number`; default = `X:max()`)
         """
-
         X = np.squeeze(X)
         assert X.ndim == 2, 'X should be two-dimensional'
 
@@ -885,7 +909,7 @@ class Visdom(object):
         - `opts.xmax`    : clip maximum value (`number`; default = `X:max()`)
         """
 
-        self._surface(X=X, stype='surface', opts=opts, win=win, env=env)
+        return self._surface(X=X, stype='surface', opts=opts, win=win, env=env)
 
     def contour(self, X, win=None, env=None, opts=None):
         """
@@ -899,7 +923,7 @@ class Visdom(object):
         - `opts.xmax`    : clip maximum value (`number`; default = `X:max()`)
         """
 
-        self._surface(X=X, stype='contour', opts=opts, win=win, env=env)
+        return self._surface(X=X, stype='contour', opts=opts, win=win, env=env)
 
     def quiver(self, X, Y, gridX=None, gridY=None,
                             win=None, env=None, opts=None):

.gitignore
@@ -1,1 +1,4 @@
 node_modules
+build
+th/CMakeLists.txt
+th/build.luarocks

example/demo1.lua
@@ -18,7 +18,10 @@ local visdom = require 'visdom'
 local plot = visdom{server = 'http://localhost', port = 8097}
 
 -- text box demo:
-local textwindow = plot:text{text = 'Hello, world!'}
+local textwindow = plot:text{
+    text = 'Hello, world! If I\'m still open, close failed'
+}
+plot:py_func{func='text', args={'Hello, world!'}}
 
 -- image demo:
 plot:image{
@@ -83,7 +86,6 @@ plot:updateTrace{                             -- add new trace to scatter plot
    Y = torch.randn(255),
    win = id,
    name = 'new trace',
-   options = {markercolor = torch.zeros(255):random(0, 255)}
 }
 
 -- 2D scatter plot with custom colors:
@@ -166,6 +168,12 @@ local id = plot:line{
    Y = torch.cat(torch.range(0, 10), torch.range(0, 10) + 5, 2),
    X = torch.cat(torch.range(0, 10), torch.range(0, 10), 2),
    options = {markers = false}
+}
+
+plot:py_func{
+  func='line',
+  args = {torch.randn(10)},
+  kwargs = {options = {title = 'This is lua through python'}}
 }
 
 -- update trace demos:

py/server.py
@@ -14,12 +14,14 @@ from __future__ import unicode_literals
 import math
 import json
 import logging
+import traceback
 import os
 import inspect
 import time
 import getpass
 import argparse
 import copy
+import visdom
 
 from os.path import expanduser
 from zmq.eventloop import ioloop
@@ -125,8 +127,8 @@ class Application(tornado.web.Application):
             (r"/socket", SocketHandler, dict(state=state, subs=subs)),
             (r"/env/(.*)", EnvHandler, dict(state=state, subs=subs)),
             (r"/save", SaveHandler, dict(state=state, subs=subs)),
-            (r"/error/(.*)", ErrorHandler),
-            (r"/(.*)", IndexHandler, dict(state=state)),
+            (r"/error/(.*)", ErrorHandler, dict(state=state, subs=subs)),
+            (r"/(.*)", IndexHandler, dict(state=state, subs=subs)),
         ]
         super(Application, self).__init__(handlers, **tornado_settings)
 
@@ -211,7 +213,9 @@ def pane(args):
 
     opts = args['opts']
 
-    return {
+    ptype = args['data'][0]['type']
+
+    p = {
         'command': 'pane',
         'id': str(uid),
         'title': opts.get('title', ''),
@@ -220,6 +224,14 @@ def pane(args):
         'height': opts.get('height'),
         'contentID': get_rand_id(),   # to detected updated panes
     }
+
+    if ptype in ['image', 'text']:
+        p.update(dict(content=args['data'][0]['content'], type=ptype))
+    else:
+        p['content'] = dict(data=args['data'], layout=args['layout'])
+        p['type'] = 'plot'
+
+    return p
 
 
 def broadcast(self, msg, eid):
@@ -244,23 +256,61 @@ def register_pane(self, p, eid):
     self.write(p['id'])
 
 
+def unpack_lua(req_args):
+    if req_args['is_table']:
+        if isinstance(req_args['val'], dict):
+            return {k: unpack_lua(v) for (k, v) in req_args['val'].items()}
+        else:
+            return [unpack_lua(v) for v in req_args['val']]
+    elif req_args['is_tensor']:
+        return visdom.from_t7(req_args['val'], b64=True)
+    else:
+        return req_args['val']
+
+
 class PostHandler(BaseHandler):
     def initialize(self, state, subs):
         self.state = state
         self.subs = subs
+        self.vis = visdom.Visdom(port=FLAGS.port, send=False)
+        self.handlers = {
+            'update': UpdateHandler,
+            'save': SaveHandler,
+            'close': CloseHandler,
+        }
+
+    def func(self, req):
+        args, kwargs = req['args'], req.get('kwargs', {})
+
+        args = (unpack_lua(a) for a in args)
+
+        for k in kwargs:
+            v = kwargs[k]
+            kwargs[k] = unpack_lua(v)
+
+        func = getattr(self.vis, req['func'])
+
+        return func(*args, **kwargs)
 
     def post(self):
-        args = tornado.escape.json_decode(tornado.escape.to_basestring(self.request.body))
+        req = tornado.escape.json_decode(
+            tornado.escape.to_basestring(self.request.body)
+        )
 
-        ptype = args['data'][0]['type']
-        p = pane(args)
-        eid = extract_eid(args)
+        if req.get('func') is not None:
+            try:
+                req, endpoint = self.func(req)
+                if (endpoint != 'events'):
+                    # Process the request using the proper handler
+                    self.handlers[endpoint].wrap_func(self, req)
+                    return
+            except Exception:
+                # get traceback and send it back
+                print(traceback.format_exc())
+                return self.write(traceback.format_exc())
 
-        if ptype in ['image', 'text']:
-            p.update(dict(content=args['data'][0]['content'], type=ptype))
-        else:
-            p['content'] = dict(data=args['data'], layout=args['layout'])
-            p['type'] = 'plot'
+        eid = extract_eid(req)
+        p = pane(req)
 
         register_pane(self, p, eid)
 
@@ -270,7 +320,8 @@ class UpdateHandler(BaseHandler):
         self.state = state
         self.subs = subs
 
-    def update(self, p, args):
+    @staticmethod
+    def update(p, args):
         pdata = p['content']['data']
 
         new_data = args['data']
@@ -304,25 +355,31 @@ class UpdateHandler(BaseHandler):
 
         return p
 
-    def post(self):
-        args = tornado.escape.json_decode(tornado.escape.to_basestring(self.request.body))
+    @staticmethod
+    def wrap_func(handler, args):
         eid = extract_eid(args)
 
-        if args['win'] not in self.state[eid]['jsons']:
-            self.write('win does not exist')
+        if args['win'] not in handler.state[eid]['jsons']:
+            handler.write('win does not exist')
             return
 
-        p = self.state[eid]['jsons'][args['win']]
+        p = handler.state[eid]['jsons'][args['win']]
 
         if not p['content']['data'][0]['type'] == 'scatter':
-            self.write('win is not scatter')
+            handler.write('win is not scatter')
             return
 
-        p = self.update(p, args)
+        p = UpdateHandler.update(p, args)
 
         p['contentID'] = get_rand_id()
-        broadcast(self, p, eid)
-        self.write(p['id'])
+        broadcast(handler, p, eid)
+        handler.write(p['id'])
+
+    def post(self):
+        args = tornado.escape.json_decode(
+            tornado.escape.to_basestring(self.request.body)
+        )
+        self.wrap_func(self, args)
 
 
 class CloseHandler(BaseHandler):
@@ -330,18 +387,23 @@ class CloseHandler(BaseHandler):
         self.state = state
         self.subs = subs
 
-    def post(self):
-        args = tornado.escape.json_decode(tornado.escape.to_basestring(self.request.body))
-
+    @staticmethod
+    def wrap_func(handler, args):
         eid = extract_eid(args)
         win = args.get('win')
 
-        keys = list(self.state[eid]['jsons'].keys()) if win is None else [win]
+        keys = list(handler.state[eid]['jsons'].keys()) if win is None else [win]
         for win in keys:
-            self.state[eid]['jsons'].pop(win, None)
+            handler.state[eid]['jsons'].pop(win, None)
             broadcast(
-                self, json.dumps({'command': 'close', 'data': win}), eid
+                handler, json.dumps({'command': 'close', 'data': win}), eid
             )
+
+    def post(self):
+        args = tornado.escape.json_decode(
+            tornado.escape.to_basestring(self.request.body)
+        )
+        self.wrap_func(self, args)
 
 
 def load_env(state, eid, socket):
@@ -392,7 +454,9 @@ class EnvHandler(BaseHandler):
         )
 
     def post(self, args):
-        sid = tornado.escape.json_decode(tornado.escape.to_basestring(self.request.body))['sid']
+        sid = tornado.escape.json_decode(
+            tornado.escape.to_basestring(self.request.body)
+        )['sid']
         load_env(self.state, args, self.subs[sid])
 
 
@@ -401,15 +465,22 @@ class SaveHandler(BaseHandler):
         self.state = state
         self.subs = subs
 
-    def post(self):
-        envs = tornado.escape.json_decode(tornado.escape.to_basestring(self.request.body))['data']
+    @staticmethod
+    def wrap_func(handler, args):
+        envs = args['data']
         envs = [escape_eid(eid) for eid in envs]
-        ret = serialize_env(self.state, envs)  # this ignores invalid env ids
-        self.write(json.dumps(ret))
+        ret = serialize_env(handler.state, envs)  # this ignores invalid env ids
+        handler.write(json.dumps(ret))
+
+    def post(self):
+        args = tornado.escape.json_decode(
+            tornado.escape.to_basestring(self.request.body)
+        )
+        self.wrap_func(self, args)
 
 
 class IndexHandler(BaseHandler):
-    def initialize(self, state):
+    def initialize(self, state, subs):
         self.state = state
 
     def get(self, args, **kwargs):

setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 readme = open('README.md').read()
 
@@ -9,6 +9,7 @@ requirements = [
     'tornado',
     'pyzmq',
     'six',
+    'torchfile',
 ]
 
 setup(

commit 6f7f46d72c5900b851dba89370ead448d724076e

py: use binary flag in from_t7
Summary:
This fixes a regression introduced in 1753e20f4ba805cb94d3646d472c53bc8d990a2a. At least on Python 3.5, files need to be opened with the binary flag to be able to read/write `bytes` objects from/to them.

I guess this commit was only tested on Python 2.7, where there is no distinction between text and binary files.

A typical backtrace looks like this:
```
Traceback (most recent call last):
  File "/home/schwarzm/.local/lib/python3.5/site-packages/visdom-0.1.6.3-py3.5.egg/visdom/server.py", line 312, in post
    req, endpoint = self.func(req)
  File "/home/schwarzm/.local/lib/python3.5/site-packages/visdom-0.1.6.3-py3.5.egg/visdom/server.py", line 299, in func
    kwargs[k] = unpack_lua(v)
  File "/home/schwarzm/.local/lib/python3.5/site-packages/visdom-0.1.6.3-py3.5.egg/visdom/server.py", line 275, in unpack_lua
    return visdom.from_t7(req_args['val'], b64=True)
  File "/home/schwarzm/.local/lib/python3.5/site-packages/visdom-0.1.6.3-py3.5.egg/visdom/__init__.py", line 66, in from_t7
    ff.write(t)
TypeError: write() argument must be str, not bytes
```
Closes https://github.com/facebookresearch/visdom/pull/215

Differential Revision: D6485461

Pulled By: JackUrb

fbshipit-source-id: 61d385a6da5959733cc4ab20c070b6fc56f7c9a2

py/__init__.py
@@ -57,11 +57,11 @@ def from_t7(t, b64=False):
     if b64:
         t = base64.b64decode(t)
 
-    with open('/dev/shm/t7', 'w') as ff:
+    with open('/dev/shm/t7', 'wb') as ff:
         ff.write(t)
         ff.close()
 
-    sf = open('/dev/shm/t7')
+    sf = open('/dev/shm/t7', 'rb')
 
     return torchfile.T7Reader(sf).read_obj()
 