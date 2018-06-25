2305c7ceca35e19a4d8411556ed2f8d535a37fe8
Jon Malmaud <malmaud@gmail.com>
2018-06-25 09:42:13 -0700

    Switch from 'setattr' to decorator. (#392)
    Summary:
    First pass at resolving #389.
    Closes https://github.com/facebookresearch/visdom/pull/392
    
    Reviewed By: lvdmaaten
    
    Differential Revision: D8543559
    
    Pulled By: JackUrb
    
    fbshipit-source-id: 6070599d5c9ed85c9ccc2b3dc34d6ca6582f5115

--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@ from setuptools import setup
 from pkg_resources import get_distribution, DistributionNotFound
 
 
+try:
+    import torch
+    if (torch.__version__ < "0.3.1"):
+        print(
+            "[visdom] WARNING: Visdom support for pytorch less than version "
+            "0.3.1 is unsupported. Visdom will still work for other purposes "
+            "though."
+        )
+except Exception:
+    pass  # User doesn't have torch
+
+
 def get_dist(pkgname):
     try:
         return get_distribution(pkgname)
     except DistributionNotFound:
         return None
-
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -34,7 +45,7 @@ setup(
     # Metadata
     name='visdom',
     version=version,
-    author='Allan Jabri, Jack Urbanek, Laurens van der Maaten',
+    author='Jack Urbanek, Allan Jabri, Laurens van der Maaten',
     author_email='jju@fb.com',
     url='https://github.com/facebookresearch/visdom',
     description='A tool for visualizing live, rich data for Torch and Numpy',
