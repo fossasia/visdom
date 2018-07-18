b72b5c085ae97ffea743d631895597240e75a7fd
Jack Urbanek <jju@fb.com>
2018-07-18 11:20:59 -0700

    Allows 'append' on non-existing plot for option re-use (#391)
    Summary:
    Closes #100.
    
    People want a blankboard so that they can use the same command in every call of an evaluation loop to plot rather than needing to instantiate the graph before they can update. Existing workarounds are doing a regular plot call on the first iteration and appends for all others, or initializing the plot with a garbage value (like 0,0 on a line) and then appending from that point. Having an option that can create or append solves the issue.
    
    This leverages `win_exists` to determine whether a particular call should be a create or append when passed the `'createappend'` option. It then does the correct action. Tested for both line and scatter, and existing behavior is maintained.
    Pull Request resolved: https://github.com/facebookresearch/visdom/pull/391
    
    Differential Revision: D8896449
    
    Pulled By: JackUrb
    
    fbshipit-source-id: 45ba7a2ecc0188deecd39df75a68b8dfe795f8fc

--- py/visdom/__init__.py
+++ py/visdom/__init__.py
@@ -694,7 +694,7 @@ class Visdom(object):
         """
         This function draws a Plotly 'Figure' object. It does not explicitly take options as it assumes you have already explicitly configured the figure's layout.
 
-        Note: You must have the 'plotly' Python package installed to use this function. 
+        Note: You must have the 'plotly' Python package installed to use this function.
         """
         try:
             import plotly
@@ -938,6 +938,8 @@ class Visdom(object):
         delete the trace that is specified in `name`. If updating a single
         trace, use `name` to specify the name of the trace to be updated.
         Update data that is all NaN is ignored (can be used for masking update).
+        Using `update='append'` will create a plot if it doesn't exist
+        and append to the existing plot otherwise.
 
         The following `opts` are supported:
 
@@ -962,7 +964,13 @@ class Visdom(object):
             return self._send(data_to_send, endpoint='update')
 
         elif update is not None:
-            assert win is not None
+            assert win is not None, 'Must define a window to update'
+
+            if update == 'append':
+                if win is None or not self.win_exists(win, env):
+                    update = None
+                else:
+                    update = 'append'
 
             # case when X is 1 dimensional and corresponding values on y-axis
             # are passed in parameter Y
@@ -1095,6 +1103,8 @@ class Visdom(object):
         delete the trace that is specified in `name`. If updating a
         single trace, use `name` to specify the name of the trace to be updated.
         Update data that is all NaN is ignored (can be used for masking update).
+        Using `update='append'` will create a plot if it doesn't exist
+        and append to the existing plot otherwise.
 
         The following `opts` are supported:
 
