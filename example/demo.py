e258d36a0c13b94c2f430d2ebdd805c484b1baa6
Jack Urbanek <jju@fb.com>
2018-08-31 11:34:22 -0700

    Adding support for modifying 3d plot scenes (#461)
    Summary:
    It turns out plotly uses a completely different format for options for 3d plots than it does for 2d plots, so we weren't supporting those properly.
    
    Resolves https://github.com/facebookresearch/visdom/issues/451
    
    We can now produce 3d plots with custom axes. Rest of the `demo.py` script works as it did before this change.
    
    <img width="382" alt="screen shot 2018-08-29 at 9 52 24 pm" src="https://user-images.githubusercontent.com/1276867/44824925-0a5cb900-abd6-11e8-9831-44abbd14a6fe.png">
    
    <!--- What types of changes does your code introduce? Put an `x` in all the boxes that apply: -->
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
    
    <!--- Go over all the following points, and put an `x` in all the boxes that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're here to help! -->
    - [x] My code follows the code style of this project.
    - [x] My change requires a change to the documentation.
    - [ ] I have updated the documentation accordingly.
    - [ ] For JavaScript changes, I have re-generated the minified JavaScript code.
    Pull Request resolved: https://github.com/facebookresearch/visdom/pull/461
    
    Differential Revision: D9594998
    
    Pulled By: JackUrb
    
    fbshipit-source-id: 93824cda79a1db8ad66395c0a380aa7d84e0265e

--- example/demo.py
+++ example/demo.py
@@ -138,12 +138,23 @@ try:
         ),
     )
 
+    # 3d scatterplot with custom labels and ranges
     viz.scatter(
         X=np.random.rand(100, 3),
         Y=(Y + 1.5).astype(int),
         opts=dict(
             legend=['Men', 'Women'],
             markersize=5,
+            xtickmin=0,
+            xtickmax=2,
+            xlabel='Arbitrary',
+            xtickvals=[0, 0.75, 1.6, 2],
+            ytickmin=0,
+            ytickmax=2,
+            ytickstep=0.5,
+            ztickmin=0,
+            ztickmax=1,
+            ztickstep=0.5,
         )
     )
 
@@ -405,7 +416,6 @@ try:
         ),
         update='append',
         win=win)
-
 
     # mesh plot
     x = [0, 0, 1, 1, 0, 0, 1, 1]
