200b316aae51fa364a11023d13e111881dc223e5
Jack Urbanek <jju@fb.com>
2019-01-11 15:30:01 -0500

    Adding missing copyrights, fix react-dom vulnerability

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+# Copyright 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import os
 from setuptools import setup
 from pkg_resources import get_distribution, DistributionNotFound
