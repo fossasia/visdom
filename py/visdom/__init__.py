Fix label size computation

py/visdom/__init__.py
@@ -1227,13 +1227,17 @@ class Visdom(object):
         if Y is not None:
             Y = np.ravel(Y)
             assert X.shape[0] == Y.shape[0], 'sizes of X and Y should match'
+            assert np.equal(np.mod(Y, 1), 0).all(), 'labels should be integers'
+            assert Y.min() >= 1, 'labels are assumed to be at least 1'
+            labels = np.unique(Y.astype(int, copy=False))
+            assert len(labels) == 1 or name is None, \
+                'name should not be specified with multiple labels or lines'
+            K = int(Y.max())  # largest label
         else:
             Y = np.ones(X.shape[0], dtype=int)
+            labels = np.ones(1, dtype=int)
+            K = 1  # largest label
 
-        assert np.equal(np.mod(Y, 1), 0).all(), 'labels should be integers'
-        assert Y.min() >= 1, 'labels are assumed to be between 1 and K'
-
-        K = int(Y.max())
         is3d = X.shape[1] == 3
 
         opts = {} if opts is None else opts
@@ -1276,12 +1280,12 @@ class Visdom(object):
         mc = opts.get('markercolor')
         lc = opts.get('linecolor')
 
-        for k in range(1, K + 1):
+        for k in labels:
             ind = np.equal(Y, k)
             if ind.any():
                 if 'legend' in opts:
                     trace_name = opts.get('legend')[k - 1]
-                elif K == 1 and name is not None:
+                elif len(labels) == 1 and name is not None:
                     trace_name = name
                 else:
                     trace_name = str(k)
@@ -1319,10 +1323,6 @@ class Visdom(object):
                     _data.update(trace_opts[trace_name])
 
                 data.append(_scrub_dict(_data))
-
-        if len(data) > 1:
-            assert name is None, \
-                'name should not be specified with multiple labels or lines'
 
         if opts:
             for marker_prop in ['markercolor']: