923ba5ebf6d36264ad780fcd6048b1b0ac367dbe
Jack Urbanek <jju@fb.com>
2019-06-12 18:53:45 -0400

    Adding optional polling-based comms to frontend

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   output: {
     filename: 'py/visdom/static/js/main.js',
   },
+  node: {
+    net: 'empty',
+    dns: 'empty',
+  },
   module: {
     loaders: [
       {
@@ -29,7 +33,10 @@ module.exports = {
         test: /\.css$/,
         loaders: ['style', 'css'],
       },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
     ],
   },
-  plugins: [new webpack.BannerPlugin('@generated')],
 };
