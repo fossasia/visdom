f6facb5e3f5269931e6fa9a532e5af2fe11a1cf0
Ubaidullah Khan <33365560+ubaidsworld@users.noreply.github.com>
2020-04-12 03:03:58 +0530

    Updated file with Latex Demo

--- example/demo.py
+++ example/demo.py
@@ -492,12 +492,12 @@ def run_demo(viz):
         win="latex_support",
         name=r'$\alpha_{1c} = 352 \pm 11 \text{ km s}^{-1}$',
         update='append',
-        opts=dict(
-        showlegend=True,
-        title="Demo Latex in Visdom",
-        xlabel=r'$\sqrt{(n_\text{c}(t|{T_\text{early}}))}$',
-        ylabel=r'$d, r \text{ (solar radius)}$',
-        )
+        opts={
+            'showlegend':True,
+            'title':"Demo Latex in Visdom",
+            'xlabel':r'$\sqrt{(n_\text{c}(t|{T_\text{early}}))}$',
+            'ylabel':r'$d, r \text{ (solar radius)}$',
+        }
     )
     viz.line(
         X=[1, 2, 3, 4],
