fossasia/visdom

commit cb74886748718ab254c3673ca598012622a0b8cb

Relicensing Visdom

setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017-present, Facebook, Inc.
+# Copyright 2017-present, The Visdom Authors
 # All rights reserved.
 #
 # This source code is licensed under the license found in the

AUTHORS
@@ -1,0 +1,7 @@
+# This is the list of Visdom's significant contributors.
+#
+# This does not necessarily list everyone who has contributed code,
+# especially since many employees of one corporation may be contributing.
+# To see the full list of contributors, see the revision history in
+# source control.
+Facebook, Inc.

example/demo-properties.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017-present, Facebook, Inc.
+# Copyright 2017-present, The Visdom Authors
 # All rights reserved.
 #
 # This source code is licensed under the license found in the

example/demo.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017-present, Facebook, Inc.
+# Copyright 2017-present, The Visdom Authors
 # All rights reserved.
 #
 # This source code is licensed under the license found in the

example/mnist-embeddings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2017-present, Facebook, Inc.
+# Copyright 2017-present, The Visdom Authors
 # All rights reserved.
 #
 # This source code is licensed under the license found in the

js/EmbeddingsPane.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-present, Facebook, Inc.
+ * Copyright 2017-present, The Visdom Authors
  * All rights reserved.
  *
  * This source code is licensed under the license found in the

js/EventSystem.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2017-present, Facebook, Inc.
+ * Copyright 2017-present, The Visdom Authors
  * All rights reserved.
  *
  * This source code is licensed under the license found in the

commit 20e5415410473a2562640908fca13aafd8f69025

Update the license metadata
Fixes: commit cb74886748718ab254c3673ca598012622a0b8cb

setup.py
@@ -59,7 +59,7 @@ setup(
     url='https://github.com/facebookresearch/visdom',
     description='A tool for visualizing live, rich data for Torch and Numpy',
     long_description=readme,
-    license='CC-BY-NC-4.0',
+    license='Apache-2.0',
 
     # Package info
     packages=['visdom'],

th/visdom-scm-1.rockspec
@@ -11,7 +11,7 @@ description = {
       A tool for visualizing live, rich data for Torch and Numpy.
    ]],
    homepage = "https://github.com/facebookresearch/visdom",
-   license = "Creative Commons Attribution-NonCommercial 4.0 International Public License"
+   license = "Apache 2.0"
 }
 
 dependencies = {