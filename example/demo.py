414e77beefb1dd47caf0cefaf49abd25417292fa
Mudita Sharma <pulkit10251.sh@gmail.com>
2021-11-23 18:44:31 +0530

    Added graph demo in demo.py

--- example/demo.py
+++ example/demo.py
@@ -571,6 +571,13 @@ def run_demo(viz):
         pass
     input('Waiting for callbacks, press enter to quit.')
 
+    # plot network graph
+
+    edges = [(0,1),(0,2),(1,3),(1,4),(1,5),(4,5)]
+    edgeLabels = [ "A", "B", "C", "D", "E", "F"]    # in the order of edges
+    nodeLabels = ["Orange", "Mango", "Apple", "Grapes", "Papaya","kiwi"]
+    
+    viz.graph(edges, edgeLabels, nodeLabels, opts = {"showEdgeLabels" : True, "showVertexLabels" : True, "scheme" : "different", "directed" : False})
 
 if __name__ == '__main__':
     DEFAULT_PORT = 8097
