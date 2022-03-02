2fbcb58fb3303b4fea2f21d3e7ab85b483c8ab98
David Hartmann <dhartmann@uni-mainz.de>
2022-03-02 17:21:12 +0100

    split webpack config into webpack.common and webpack.prod

---
+++ webpack.prod.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright 2017-present, The Visdom Authors
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+});
