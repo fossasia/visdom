2af1b515b69266a47e7eb97794afe1693036f67c
David Hartmann <dhartmann@uni-mainz.de>
2022-03-02 17:22:06 +0100

    add webpack.dev config to improve debugging

---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2017-present, The Visdom Authors
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ */
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+});
