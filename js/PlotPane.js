26dfc71b1f235990e919c121eae36fdd7d1b62ba
David Hartmann <dhartmann@uni-mainz.de>
2022-03-21 22:10:12 +0100

    added smooth line plot functionality to js/PlotPane

--- js/PlotPane.js
+++ js/PlotPane.js
@@ -9,12 +9,30 @@
 
 import React from 'react';
 const Pane = require('./Pane');
+const { sgg } = require('ml-savitzky-golay-generalized');
 
 class PlotPane extends React.Component {
   _paneRef = null;
   _plotlyRef = null;
   _width = null;
   _height = null;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxsmoothvalue: 100,
+      smoothWidgetActive: false,
+      smoothvalue: 1,
+    };
+  }
+
+  toggleSmoothWidget = () => {
+    this.setState(state => ({ smoothWidgetActive: !state.smoothWidgetActive }));
+  };
+
+  updateSmoothSlider = value => {
+    this.setState(state => ({ smoothvalue: value }));
+  };
 
   componentDidMount() {
     this.newPlot();
@@ -51,21 +69,79 @@ class PlotPane extends React.Component {
     this.newPlot();
   }
 
-  shouldComponentUpdate(nextProps) {
+  shouldComponentUpdate(nextProps, nextState) {
     if (this.props.contentID !== nextProps.contentID) {
       return true;
     } else if (this.props.h !== nextProps.h || this.props.w !== nextProps.w) {
       return true;
     } else if (this.props.isFocused !== nextProps.isFocused) {
       return true;
+    } else if (this.state.smoothWidgetActive !== nextState.smoothWidgetActive) {
+      return true;
+    } else if (this.state.smoothvalue !== nextState.smoothvalue) {
+      return true;
     }
+
     return false;
   }
 
   newPlot = () => {
+    var data = this.props.content.data;
+
+    // add smoothed line plots for existing line plots
+    var smooth_data = [];
+    if (this.state.smoothWidgetActive) {
+      smooth_data = data
+        .filter(d => d['type'] == 'scatter' && d['mode'] == 'lines')
+        .map((d, dataId) => {
+          var smooth_d = JSON.parse(JSON.stringify(d));
+          var windowSize = 2 * this.state.smoothvalue + 1;
+
+          // remove legend of smoothed plot
+          smooth_d.showlegend = false;
+
+          // turn off smoothing for smoothvalue of 3 or too small arrays
+          if (windowSize < 5 || smooth_d.x.length <= 5) {
+            d.opacity = 1.0;
+
+            return smooth_d;
+          }
+
+          // savitzky golay requires the window size to be ≥ 5
+          windowSize = Math.max(windowSize, 5);
+
+          // window size needs to be odd
+          if (smooth_d.x.length % 2 == 0)
+            windowSize = Math.min(windowSize, smooth_d.x.length - 1);
+          else windowSize = Math.min(windowSize, smooth_d.x.length);
+          smooth_d.y = sgg(smooth_d.y, smooth_d.x, { windowSize: windowSize });
+
+          // adapt color & transparency
+          d.opacity = 0.35;
+          smooth_d.opacity = 1.0;
+          smooth_d.marker.line.color = 0;
+
+          return smooth_d;
+        });
+
+      // pad data in case we have some smoothed lines
+      // this is to let plotly use the same colors if no colors are given by the user
+      if (smooth_data.length > 0) {
+        data = Array.from(data);
+        let num_to_fill = 10 - (data.length % 10);
+        for (let i = 0; i < num_to_fill; i++) data.push({});
+        console.log(data);
+      }
+    } else
+      this.props.content.data
+        .filter(data => data['type'] == 'scatter' && data['mode'] == 'lines')
+        .map((d, dataId) => {
+          d.opacity = 1.0;
+        });
+
     Plotly.newPlot(
       this.props.contentID,
-      this.props.content.data,
+      data.concat(smooth_data),
       this.props.content.layout,
       { showLink: true, linkText: 'Edit' }
     );
@@ -83,11 +159,52 @@ class PlotPane extends React.Component {
   };
 
   render() {
+    // check if data can be smoothed
+    var contains_line_plots = this.props.content.data.some((data, dataId) => {
+      return data['type'] == 'scatter' && data['mode'] == 'lines';
+    });
+
+    var smooth_widget_button = '';
+    var smooth_widget = '';
+    if (contains_line_plots) {
+      smooth_widget_button = (
+        <button
+          key="smooth_widget_button"
+          title="smooth lines"
+          onClick={this.toggleSmoothWidget}
+          className={
+            this.state.smoothWidgetActive ? 'pull-right active' : 'pull-right'
+          }
+        >
+          ~
+        </button>
+      );
+      if (this.state.smoothWidgetActive) {
+        smooth_widget = (
+          <div className="widget" key="smooth_widget">
+            <div style={{ display: 'flex' }}>
+              <span>Smoothing:&nbsp;&nbsp;</span>
+              <input
+                type="range"
+                min="1"
+                max={this.state.maxsmoothvalue}
+                value={this.state.smoothvalue}
+                onInput={ev => this.updateSmoothSlider(ev.target.value)}
+              />
+              <span>&nbsp;&nbsp;{this.state.selected}&nbsp;&nbsp;</span>
+            </div>
+          </div>
+        );
+      }
+    }
+
     return (
       <Pane
         {...this.props}
         handleDownload={this.handleDownload}
         ref={ref => (this._paneRef = ref)}
+        barwidgets={[smooth_widget_button]}
+        widgets={[smooth_widget]}
         enablePropertyList
       >
         <div
