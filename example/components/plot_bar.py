e921b039f8534a0fae968d5a47a02afe47716d01
David Hartmann <dhartmann@uni-mainz.de>
2022-04-21 18:10:57 +0200

    test: added opts.title values to enable compare-view testing

--- example/components/plot_bar.py
+++ example/components/plot_bar.py
@@ -1,39 +1,46 @@
 import numpy as np
 
 def plot_bar_basic(viz, env, args):
-    viz.bar(X=np.random.rand(20), env=env)
+    opts = dict(title=args[0]) if len(args) > 0 else {}
+    viz.bar(X=np.random.rand(20), opts=opts, env=env)
 
 def plot_bar_stacked(viz, env, args):
+    title = args[0] if len(args) > 0 else None
     viz.bar(
         X=np.abs(np.random.rand(5, 3)),
         opts=dict(
             stacked=True,
             legend=['Facebook', 'Google', 'Twitter'],
-            rownames=['2012', '2013', '2014', '2015', '2016']
+            rownames=['2012', '2013', '2014', '2015', '2016'],
+            title=title
         ),
         env=env
     )
 
 def plot_bar_nonstacked(viz, env, args):
+    title = args[0] if len(args) > 0 else None
     viz.bar(
         X=np.random.rand(20, 3),
         opts=dict(
             stacked=False,
-            legend=['The Netherlands', 'France', 'United States']
+            legend=['The Netherlands', 'France', 'United States'],
+            title=title
         ),
         env=env
     )
 
 # histogram
 def plot_bar_histogram(viz, env, args):
-    viz.histogram(X=np.random.rand(10000), opts=dict(numbins=20), env=env)
+    title = args[0] if len(args) > 0 else None
+    viz.histogram(X=np.random.rand(10000), opts=dict(numbins=20, title=title), env=env)
 
 # pie chart
 def plot_bar_piechart(viz, env, args):
+    title = args[0] if len(args) > 0 else None
     X = np.asarray([19, 26, 55])
     viz.pie(
         X=X,
-        opts=dict(legend=['Residential', 'Non-Residential', 'Utility']),
+        opts=dict(legend=['Residential', 'Non-Residential', 'Utility'], title=title),
         env=env
     )
 
