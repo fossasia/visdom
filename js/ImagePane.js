f111d65b1f0e84bc6e9e970da691de5b49c57b04
David Hartmann <dhartmann@uni-mainz.de>
2022-05-29 16:14:10 +0200

    hooks: refactor ImagePane

--- js/ImagePane.js
+++ js/ImagePane.js
@@ -7,88 +7,76 @@
  *
  */
 
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import EventSystem from './EventSystem';
 import Pane from './Pane';
 
 const DEFAULT_HEIGHT = 400;
 const DEFAULT_WIDTH = 300;
 
-class ImagePane extends React.Component {
-  _paneRef = null;
-  _imgRef = null;
-  _natHeight = null;
-  _natWidth = null;
+function ImagePane(props) {
+  const paneRef = useRef();
+  const imgRef = useRef();
+  const [view, setView] = useState({ scale: 1, tx: 0, ty: 0 });
+  const [imgDim, setImgDim] = useState({ width: null, height: 0 });
+  const [selected, setSelected] = useState(props.selected);
+  const [mouseLocation, setMouseLocation] = useState({
+    x: 0,
+    y: 0,
+    visibility: 'hidden',
+  });
+  const [dragStart, setDragStart] = useState({
+    x: 0,
+    y: 0,
+  });
 
-  state = {
-    lastModTime: 0,
-    scale: 1,
-    tx: 0,
-    ty: 0,
-    selected: this.props.selected,
-    mouse_location: { x: 0, y: 0, visibility: 'hidden' },
-  };
+  useEffect(() => {
+    setSelected(props.selected);
+  }, [props.selected]);
+  useEffect(() => {
+    // Reset the image settings when the user resizes the window. Avoid
+    // constantly resetting the zoom level when user has not zoomed.
+    if (Math.abs(view['scale'] - 1) > Number.EPSILON) handleReset();
+  }, [props.width, props.height]);
 
-  drag_start_x = null;
-  drag_start_y = null;
+  // initialize
+  useEffect(() => {
+    const onEvent = (event) => {
+      switch (event.type) {
+        case 'keydown':
+        case 'keypress':
+          event.preventDefault();
+          break;
+        case 'keyup':
+          props.appApi.sendPaneMessage({
+            event_type: 'KeyPress',
+            key: event.key,
+            key_code: event.keyCode,
+          });
+          break;
+        case 'click':
+          props.appApi.sendPaneMessage({
+            event_type: 'Click',
+            image_coord: mouseLocation,
+          });
+          break;
+      }
+    };
 
-  componentWillReceiveProps = (nextProps) => {
-    if (nextProps.selected !== this.props.selected) {
-      this.setState({ selected: nextProps.selected });
-    }
-    if (
-      (nextProps.width != this.props.width ||
-        nextProps.height != this.props.height) &&
-      Math.abs(this.state.scale - 1) > Number.EPSILON
-    ) {
-      // Reset the image settings when the user resizes the window. Avoid
-      // constantly resetting the zoom level when user has not zoomed.
-      this.handleReset();
-    }
-  };
+    EventSystem.subscribe('global.event', onEvent);
+    return function cleanup() {
+      EventSystem.unsubscribe('global.event', onEvent);
+    };
+  }, [mouseLocation]);
 
-  onEvent = (event) => {
-    if (!this.props.isFocused) {
-      return;
-    }
-
-    switch (event.type) {
-      case 'keydown':
-      case 'keypress':
-        event.preventDefault();
-        break;
-      case 'keyup':
-        this.props.appApi.sendPaneMessage({
-          event_type: 'KeyPress',
-          key: event.key,
-          key_code: event.keyCode,
-        });
-        break;
-      case 'click':
-        this.props.appApi.sendPaneMessage({
-          event_type: 'Click',
-          image_coord: this.state.mouse_location,
-        });
-        break;
-    }
-  };
-
-  componentDidMount() {
-    EventSystem.subscribe('global.event', this.onEvent);
-  }
-
-  componentWillUnmount() {
-    EventSystem.unsubscribe('global.event', this.onEvent);
-  }
-
-  handleDownload = () => {
+  const handleDownload = () => {
     var link = document.createElement('a');
-    link.download = `${this.props.title || 'visdom_image'}.jpg`;
-    link.href = this.props.content.src;
+    link.download = `${props.title || 'visdom_image'}.jpg`;
+    link.href = props.content.src;
     link.click();
   };
 
-  handleZoom = (ev) => {
+  const handleZoom = (ev) => {
     if (ev.altKey) {
       //var direction = natural.checked ? -1 : 1;
       let direction = -1;
@@ -98,36 +86,35 @@ class ImagePane extends React.Component {
       // If shift is pressed only scroll sidewise (to allow scrolling
       // to the side by keep shift pressed and using normal scrolling
       // on the image pane)
-      if (ev.shiftKey) {
-        this.setState({
-          tx: this.state.tx + scrollDirectionY * direction * 50,
+      if (ev.shiftKey)
+        setView({
+          ...view,
+          tx: view['tx'] + scrollDirectionY * direction * 50,
         });
-      } else {
-        this.setState({
-          tx: this.state.tx + scrollDirectionX * direction * 50,
+      else
+        setView({
+          ...view,
+          tx: view['tx'] + scrollDirectionX * direction * 50,
+          ty: view['ty'] + scrollDirectionY * direction * 50,
         });
-        this.setState({
-          ty: this.state.ty + scrollDirectionY * direction * 50,
-        });
-      }
       ev.stopPropagation();
       ev.preventDefault();
     } else if (ev.ctrlKey) {
       // get the x and y offset of the pane
-      let rect = this._paneRef._windowRef.children[1].getBoundingClientRect();
+      let rect = paneRef.current.children[1].getBoundingClientRect();
       // Get browser independent scaling factor
       let scrollDirectionY = Math.sign(ev.deltaY);
       // Compute the coords of the mouse relative to the top left of the pane
       let xscreen = ev.clientX - rect.x;
       let yscreen = ev.clientY - rect.y;
       // Compute the coords of the pixel under the mouse wrt the image top left
-      let ximage = (xscreen - this.state.tx) / this.state.scale;
-      let yimage = (yscreen - this.state.ty) / this.state.scale;
-      let new_scale = this.state.scale * Math.exp(-scrollDirectionY / 10);
+      let ximage = (xscreen - view['tx']) / view['scale'];
+      let yimage = (yscreen - view['ty']) / view['scale'];
+      let new_scale = view['scale'] * Math.exp(-scrollDirectionY / 10);
       // Update the state.
       // The offset is modifed such that the pixel under the mouse
       // is the same after zooming
-      this.setState({
+      setView({
         scale: new_scale,
         tx: xscreen - new_scale * ximage,
         ty: yscreen - new_scale * yimage,
@@ -137,198 +124,193 @@ class ImagePane extends React.Component {
     }
   };
 
-  handleDragStart = (ev) => {
-    this.drag_start_x = ev.screenX;
-    this.drag_start_y = ev.screenY;
+  const handleDragStart = (ev) => {
+    setDragStart({ x: ev.screenX, y: ev.screenY });
+    ev.dataTransfer.setDragImage(new Image(), 0, 0); // disables ghost image
   };
 
-  handleDragOver = (ev) => {
-    this.setState({
-      tx: this.state.tx + ev.screenX - this.drag_start_x,
-      ty: this.state.ty + ev.screenY - this.drag_start_y,
+  const handleDragOver = (ev) => {
+    setView({
+      scale: view['scale'],
+      tx: view['tx'] + ev.screenX - dragStart.x,
+      ty: view['ty'] + ev.screenY - dragStart.y,
     });
-    this.drag_start_x = ev.screenX;
-    this.drag_start_y = ev.screenY;
+    setDragStart({ x: ev.screenX, y: ev.screenY });
   };
 
-  handleMouseOver = (ev) => {
+  const handleMouseOver = (ev) => {
     // get the x and y offset of the pane
-    var rect = this._paneRef._windowRef.children[1].getBoundingClientRect();
+    var rect = paneRef.current.children[1].getBoundingClientRect();
     // Compute the coords of the mouse relative to the top left of the pane
     var xscreen = ev.clientX - rect.x;
     var yscreen = ev.clientY - rect.y;
     // Compute the coords of the pixel under the mouse wrt the image top left
-    var ximage = Math.round((xscreen - this.state.tx) / this.state.scale);
-    var yimage = Math.round((yscreen - this.state.ty) / this.state.scale);
-    this.setState({
-      mouse_location: {
-        x: ximage,
-        y: yimage,
-        visibility: ev.altKey ? 'visible' : 'hidden',
-      },
+    var ximage = Math.round((xscreen - view['tx']) / view['scale']);
+    var yimage = Math.round((yscreen - view['ty']) / view['scale']);
+    setMouseLocation({
+      x: ximage,
+      y: yimage,
+      visibility: ev.altKey ? 'visible' : 'hidden',
     });
   };
 
-  handleReset = () => {
-    this.setState({
+  const handleReset = () => {
+    setView({
       scale: 1,
       tx: 0,
       ty: 0,
     });
   };
 
-  updateSlider = (evt) => {
+  const updateSlider = (evt) => {
     // TODO add history update events here! need to send these to the client
     // with sendPaneMessage
-    this.setState({
-      selected: evt.target.value,
-    });
+    setSelected(parseInt(evt.target.value));
   };
 
-  // Find the height that preserves the aspect ratio given 'scaledWidth'
-  computeHFromW = (scaledWidth) => {
-    return Math.ceil((this._natHeight / this._natWidth) * scaledWidth);
+  // Find the width/height that preserves the aspect ratio given 'scaledWidth/height'
+  const computeHFromW = (scaledWidth) => {
+    return Math.ceil((imgDim.height / imgDim.width) * scaledWidth);
+  };
+  const computeWFromH = (scaledHeight) => {
+    return Math.ceil((imgDim.width / imgDim.height) * scaledHeight);
   };
 
-  // Find the width that preserves the aspect ratio given 'scaledHeight'
-  computeWFromH = (scaledHeight) => {
-    return Math.ceil((this._natWidth / this._natHeight) * scaledHeight);
-  };
+  let content = props.content;
+  let type = props.type;
+  let widgets = [];
 
-  render() {
-    let content = this.props.content;
-    let type = this.props.type;
-    let widgets = [];
-
-    if (type === 'image_history') {
-      let selected = this.state.selected;
-      if (this.props.show_slider) {
-        widgets.push(
-          <div className="widget">
-            <div style={{ display: 'flex' }}>
-              <span>Selected:&nbsp;&nbsp;</span>
-              <input
-                type="range"
-                min="0"
-                max={content.length - 1}
-                value={this.state.selected}
-                onInput={this.updateSlider.bind(this)}
-              />
-              <span>&nbsp;&nbsp;{this.state.selected}&nbsp;&nbsp;</span>
-            </div>
-          </div>
-        );
-      }
-      content = content[selected];
-    }
-
-    if (content.caption) {
-      widgets.splice(0, 0, <span className="widget">{content.caption}</span>);
-    }
-
-    // TODO use this widget_height somehow to adjust window height!!!
-    let widget_height = 30 * widgets.length - 10;
-
-    const divstyle = {
-      left: this.state.tx,
-      top: this.state.ty,
-      position: 'absolute',
-    };
-
-    let candidateWidth = Math.ceil(1 + this.props.width * this.state.scale);
-    let candidateHeight = Math.ceil(1 + this.props.height * this.state.scale);
-
-    let imageContainerStyle = {
-      alignItems: 'row',
-      display: 'flex',
-      height: isNaN(candidateHeight) ? DEFAULT_HEIGHT : candidateHeight,
-      justifyContent: 'center',
-      width: isNaN(candidateWidth) ? DEFAULT_WIDTH : candidateWidth,
-    };
-
-    if (this._natHeight === null || this._natWidth === null) {
-      // Do nothing, don't change the width/height
-    } else if (candidateWidth >= candidateHeight) {
-      // If the width exceeds the height, then we use the height as the limiting
-      // factor
-      let newWidth = this.computeWFromH(candidateHeight);
-      // If the new width would exceed the window boundaries, we need to
-      // instead use the window width as the limiting factor
-      if (newWidth > candidateWidth) {
-        candidateHeight = this.computeHFromW(candidateWidth);
-        imageContainerStyle.alignItems = 'column';
-      } else {
-        candidateWidth = newWidth;
-      }
-    } else if (candidateWidth < candidateHeight) {
-      // If the height exceeds the width, then we use the width as the limiting
-      // factor
-      let newHeight = this.computeHFromW(candidateWidth);
-      // If the new height would exceed the window boundaries, we need to
-      // instead use the window height as the limiting factor
-      if (newHeight > candidateHeight) {
-        candidateWidth = this.computeWFromH(candidateHeight);
-      } else {
-        imageContainerStyle.alignItems = 'column';
-        candidateHeight = newHeight;
-      }
-    }
-
-    // During initial render cycle,
-    // Math.ceil(1 + this.props.height/width * this.state.scale) may be NaN.
-    // Set a default value here to avoid warnings, which will be updated on the
-    // next render
-
-    if (isNaN(candidateHeight)) {
-      candidateHeight = DEFAULT_HEIGHT;
-    }
-
-    if (isNaN(candidateWidth)) {
-      candidateWidth = DEFAULT_WIDTH;
-    }
-
-    return (
-      <Pane
-        {...this.props}
-        handleDownload={this.handleDownload}
-        handleReset={this.handleReset.bind(this)}
-        handleZoom={this.handleZoom.bind(this)}
-        handleMouseMove={this.handleMouseOver.bind(this)}
-        ref={(ref) => (this._paneRef = ref)}
-        widgets={widgets}
-      >
-        <div style={divstyle}>
-          <div style={imageContainerStyle}>
-            <img
-              className="content-image cssTransforms"
-              src={content.src}
-              ref={(ref) => (this._imgRef = ref)}
-              onLoad={() => {
-                if (this._natHeight === null) {
-                  this._natHeight = this._imgRef.naturalHeight;
-                }
-                if (this._natWidth === null) {
-                  this._natWidth = this._imgRef.naturalWidth;
-                }
-              }}
-              width={candidateWidth + 'px'}
-              height={candidateHeight + 'px'}
-              onDoubleClick={this.handleReset.bind(this)}
-              onDragStart={this.handleDragStart.bind(this)}
-              onDragOver={this.handleDragOver.bind(this)}
+  if (type === 'image_history') {
+    if (props.show_slider) {
+      widgets.push(
+        <div className="widget" key="image_slider">
+          <div style={{ display: 'flex' }}>
+            <span>Selected:&nbsp;&nbsp;</span>
+            <input
+              type="range"
+              min="0"
+              max={content.length - 1}
+              value={selected}
+              onChange={updateSlider}
             />
+            <span>&nbsp;&nbsp;{selected}&nbsp;&nbsp;</span>
           </div>
         </div>
-        <p className="caption">{content.caption}</p>
-        <span
-          className="mouse_image_location"
-          style={{ visibility: this.state.mouse_location.visibility }}
-        >
-          {this.state.mouse_location.x + ' / ' + this.state.mouse_location.y}
-        </span>
-      </Pane>
+      );
+    }
+    content = content[selected];
+  }
+
+  if (content.caption) {
+    widgets.splice(
+      0,
+      0,
+      <span className="widget" key="img_caption">
+        {content.caption}
+      </span>
     );
   }
+
+  // TODO use this widget_height somehow to adjust window height!!!
+  let widget_height = 30 * widgets.length - 10;
+
+  const divstyle = {
+    left: view['tx'],
+    top: view['ty'],
+    position: 'absolute',
+  };
+
+  let candidateWidth = Math.ceil(1 + props.width * view['scale']);
+  let candidateHeight = Math.ceil(1 + props.height * view['scale']);
+
+  let imageContainerStyle = {
+    alignItems: 'row',
+    display: 'flex',
+    height: isNaN(candidateHeight) ? DEFAULT_HEIGHT : candidateHeight,
+    justifyContent: 'center',
+    width: isNaN(candidateWidth) ? DEFAULT_WIDTH : candidateWidth,
+  };
+
+  if (imgDim.height === null || imgDim.width === null) {
+    // Do nothing, don't change the width/height
+  } else if (candidateWidth >= candidateHeight) {
+    // If the width exceeds the height, then we use the height as the limiting
+    // factor
+    let newWidth = computeWFromH(candidateHeight);
+    // If the new width would exceed the window boundaries, we need to
+    // instead use the window width as the limiting factor
+    if (newWidth > candidateWidth) {
+      candidateHeight = computeHFromW(candidateWidth);
+      imageContainerStyle.alignItems = 'column';
+    } else {
+      candidateWidth = newWidth;
+    }
+  } else if (candidateWidth < candidateHeight) {
+    // If the height exceeds the width, then we use the width as the limiting
+    // factor
+    let newHeight = computeHFromW(candidateWidth);
+    // If the new height would exceed the window boundaries, we need to
+    // instead use the window height as the limiting factor
+    if (newHeight > candidateHeight) {
+      candidateWidth = computeWFromH(candidateHeight);
+    } else {
+      imageContainerStyle.alignItems = 'column';
+      candidateHeight = newHeight;
+    }
+  }
+
+  // During initial render cycle,
+  // Math.ceil(1 + props.height/width * view["scale"]) may be NaN.
+  // Set a default value here to avoid warnings, which will be updated on the
+  // next render
+
+  if (isNaN(candidateHeight)) {
+    candidateHeight = DEFAULT_HEIGHT;
+  }
+
+  if (isNaN(candidateWidth)) {
+    candidateWidth = DEFAULT_WIDTH;
+  }
+
+  return (
+    <Pane
+      {...props}
+      handleDownload={handleDownload}
+      handleReset={handleReset}
+      handleZoom={handleZoom}
+      handleMouseMove={handleMouseOver}
+      ref={paneRef}
+      widgets={widgets}
+    >
+      <div style={divstyle}>
+        <div style={imageContainerStyle}>
+          <img
+            className="content-image cssTransforms"
+            src={content.src}
+            ref={imgRef}
+            onLoad={() => {
+              setImgDim({
+                height: imgRef.current.naturalHeight,
+                width: imgRef.current.naturalWidth,
+              });
+            }}
+            width={candidateWidth + 'px'}
+            height={candidateHeight + 'px'}
+            onDoubleClick={handleReset}
+            onDragStart={handleDragStart}
+            onDragOver={handleDragOver}
+          />
+        </div>
+      </div>
+      <p className="caption">{content.caption}</p>
+      <span
+        className="mouse_image_location"
+        style={{ visibility: mouseLocation.visibility }}
+      >
+        {mouseLocation.x + ' / ' + mouseLocation.y}
+      </span>
+    </Pane>
+  );
 }
 
 export default ImagePane;
