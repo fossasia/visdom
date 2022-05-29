1dd589a7f7ffadc95d9d0a55bdf76dc0a13c1187
David Hartmann <dhartmann@uni-mainz.de>
2022-05-29 16:13:48 +0200

    hooks: refactor PropertiesItem

--- js/PropertyItem.js
+++ js/PropertyItem.js
@@ -15,38 +15,30 @@ function EditablePropertyText(props) {
   const [isEdited, setIsEdited] = useState(false);
 
   // update the state to current input value (rejects events based on validateHandler)
-  const handleChange =
-    props.handleChange ||
-    (event => {
-      let newValue = event.target.value;
-      if (props.validateHandler && !props.validateHandler(newValue))
-        event.preventDefault();
-      else setActualValue(newValue);
-    });
+  const handleChange = (event) => {
+    let newValue = event.target.value;
+    if (props.validateHandler && !props.validateHandler(newValue))
+      event.preventDefault();
+    else setActualValue(newValue);
+  };
 
   // focus / blur toggles edit mode & blur saves the state
-  const onFocus =
-    props.onFocus ||
-    (event => {
-      setIsEdited(true);
-    });
-  const onBlur =
-    props.onBlur ||
-    (event => {
-      setIsEdited(false);
-      if (props.submitHandler) props.submitHandler(actualValue);
+  const onFocus = (event) => {
+    setIsEdited(true);
+  };
+  const onBlur = (event) => {
+    setIsEdited(false);
+    if (props.submitHandler) props.submitHandler(actualValue);
 
-      // prevents the pane to drop focus
-      // otherwise the sendPaneMessage-API does not work
-      event.stopPropagation();
-    });
+    // prevents the pane to drop focus
+    // otherwise the sendPaneMessage-API does not work
+    event.stopPropagation();
+  };
 
   // Enter invokes blur and thus submits the change
-  const handleKeyPress =
-    props.handleKeyPress ||
-    (event => {
-      if (event.key === 'Enter') textInput.current.blur();
-    });
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') textInput.current.blur();
+  };
 
   // adapt state if props changed & we are not in edit mode
   useEffect(() => {
@@ -66,62 +58,59 @@ function EditablePropertyText(props) {
   );
 }
 
-class PropertiesItem extends React.Component {
-  updateValue = (propId, value) => {};
-
-  renderPropertyValue = (prop, propId) => {
-    switch (prop.type) {
-      case 'text':
-        return (
-          <EditablePropertyText
-            value={prop.value}
-            submitHandler={(value) => this.updateValue(propId, value)}
+function PropertyItem(props) {
+  const updateValue = props.updateValue || ((propId, value) => {});
+  switch (props.type) {
+    case 'text':
+      return (
+        <EditablePropertyText
+          value={props.value}
+          submitHandler={(value) => updateValue(props.propId, value)}
+        />
+      );
+    case 'number':
+      return (
+        <EditablePropertyText
+          value={props.value}
+          submitHandler={(value) => updateValue(props.propId, value)}
+          validateHandler={(value) => value.match(/^[0-9]*([.][0-9]*)?$/i)}
+        />
+      );
+    case 'button':
+      return (
+        <button
+          className="btn btn-sm"
+          onClick={() => updateValue(props.propId, 'clicked')}
+        >
+          {props.value}
+        </button>
+      );
+    case 'checkbox':
+      return (
+        <label className="checkbox-inline">
+          <input
+            type="checkbox"
+            checked={props.value}
+            onChange={() => updateValue(props.propId, !props.value)}
           />
-        );
-      case 'number':
-        return (
-          <EditablePropertyText
-            value={prop.value}
-            submitHandler={(value) => this.updateValue(propId, value)}
-            validateHandler={(value) => value.match(/^[0-9]*([.][0-9]*)?$/i)}
-          />
-        );
-      case 'button':
-        return (
-          <button
-            className="btn btn-sm"
-            onClick={() => this.updateValue(propId, 'clicked')}
-          >
-            {prop.value}
-          </button>
-        );
-      case 'checkbox':
-        return (
-          <label className="checkbox-inline">
-            <input
-              type="checkbox"
-              checked={prop.value}
-              onChange={() => this.updateValue(propId, !prop.value)}
-            />
-            &nbsp;
-          </label>
-        );
-      case 'select':
-        return (
-          <select
-            className="form-control"
-            onChange={(event) => this.updateValue(propId, event.target.value)}
-            value={prop.value}
-          >
-            {prop.values.map((name, id) => (
-              <option key={id} value={id}>
-                {name}
-              </option>
-            ))}
-          </select>
-        );
-    }
-  };
+          &nbsp;
+        </label>
+      );
+    case 'select':
+      return (
+        <select
+          className="form-control"
+          onChange={(event) => updateValue(props.propId, event.target.value)}
+          value={props.value}
+        >
+          {props.values.map((name, id) => (
+            <option key={id} value={id}>
+              {name}
+            </option>
+          ))}
+        </select>
+      );
+  }
 }
 
-export default AbstractPropertiesList;
+export default PropertyItem;
