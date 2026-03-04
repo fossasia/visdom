29321300cc5357bdc8cf0d35e7cc89c2a0dc50ae
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-03-04 12:20:53 +0530

    refactor-setup-replace-pkg_resources-with-importlib.metadata

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 import os
 from io import open
 from setuptools import setup, find_packages
-from pkg_resources import get_distribution, DistributionNotFound
+from importlib.metadata import distribution, PackageNotFoundError
 
 
 try:
@@ -26,8 +26,8 @@ except Exception:
 
 def get_dist(pkgname):
     try:
-        return get_distribution(pkgname)
-    except DistributionNotFound:
+        return distribution(pkgname)
+    except PackageNotFoundError:
         return None
 
 here = os.path.abspath(os.path.dirname(__file__))
