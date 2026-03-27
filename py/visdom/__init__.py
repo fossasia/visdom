Fix incorrect normalization heuristic in viz.image() for float images

py/visdom/__init__.py
@@ -1281,8 +1281,12 @@ class Visdom(object):
             img = img[np.newaxis, :, :].repeat(3, axis=0)
 
         if "float" in str(img.dtype):
-            if img.max() <= 1:
-                img = img * 255.0
+    min_val = img.min()
+    max_val = img.max()
+
+    # Normalize only if clearly in [0,1] range AND has variation
+    if 0.0 <= min_val and max_val <= 1.0 and (max_val - min_val) > 0:
+        img = img * 255.0
             img = np.uint8(img)
 
         img = np.transpose(img, (1, 2, 0))