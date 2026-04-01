af3ce73decd5385013d76340048e740ec4164c5a
Nikita Vasant Choudhary <nikitachoudhary0801@gmail.com>
2026-04-01 19:34:32 +0530

    Update ApiProvider.js

--- js/api/ApiProvider.js
+++ js/api/ApiProvider.js
@@ -109,27 +109,27 @@ const ApiProvider = ({ children }) => {
           readonly: cmd.readonly,
         }));
         break;
-     case 'pane':
-case 'window':
-case 'window_update':
-  apiHandlers.current.onWindowMessage({
-    cmd: cmd,
-    update: cmd.command === 'window_update',
-  });
-  break;
+      case 'pane':
+      case 'window':
+      case 'window_update':
+        apiHandlers.current.onWindowMessage({
+          cmd: cmd,
+          update: cmd.command === 'window_update',
+        });
+        break;
       case 'reload':
         apiHandlers.current.onReloadMessage(cmd.data);
         break;
       case 'close':
         apiHandlers.current.onCloseMessage(cmd.data);
         break;
-      case 'layout':
-case 'layout_update':
-  apiHandlers.current.onLayoutMessage({
-    data: cmd.data,
-    update: cmd.command === 'layout_update',
-  });
-  break;
+     case 'layout':
+     case 'layout_update':
+       apiHandlers.current.onLayoutMessage({
+         data: cmd.data,
+         update: cmd.command === 'layout_update',
+         });
+         break;
       case 'env_update':
         apiHandlers.current.onEnvUpdate(cmd.data);
         break;
