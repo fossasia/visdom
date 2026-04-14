6c47cee9a01d86ca8d1716d541c89c91cfacfef0
mraniket404 <aniketgosavi471@gmail.com>
2026-04-14 13:30:18 +0530

    Fix: restore original comments in ImagePane

--- js/panes/ImagePane.js
+++ js/panes/ImagePane.js
@@ -1,6 +1,10 @@
 /**
  * Copyright 2017-present, The Visdom Authors
  * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
  */
 
 import React, { useContext, useEffect, useRef, useState } from 'react';
