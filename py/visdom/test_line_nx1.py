65370faecd56c6d72cdc4696707dbc63b9cb2251
Sumedha Ghosh <sumedhaghosh2005@gmail.com>
2026-04-28 17:59:35 +0530

    Improve Nx1 regression test: remove server dependency and mock _send

--- py/visdom/test_line_nx1.py
+++ py/visdom/test_line_nx1.py
@@ -1,10 +1,16 @@
 import numpy as np
 from visdom import Visdom
 
-def test_line_nx1_does_not_crash():
-    viz = Visdom(server="http://localhost", raise_exceptions=False)
 
-    X = np.linspace(0, 10, 5)
+def test_line_nx1_does_not_crash(monkeypatch):
+    viz = Visdom(raise_exceptions=False)
+
+    # Mock _send to avoid real network calls
+    monkeypatch.setattr(viz, "_send", lambda *args, **kwargs: True)
+
+    X = np.arange(5)
     Y = np.random.rand(5, 1)
 
-    viz.line(Y=Y, X=X, opts=dict(title="Nx1 test"))
+    result = viz.line(X=X, Y=Y)
+
+    assert result is not None
