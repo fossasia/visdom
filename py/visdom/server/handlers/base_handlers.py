a0e5f85903c9339037d6d6f6501c023549c6210a
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-05-20 19:13:06 +0530

    Update base_handlers.py

--- py/visdom/server/handlers/base_handlers.py
+++ py/visdom/server/handlers/base_handlers.py
@@ -87,13 +87,9 @@ class BaseHandler(tornado.web.RequestHandler):
                 return
             except Exception as e:
                 logging.error(e)
-            error_msg = (
-                str(exc_info[1]) if exc_info and exc_info[1] else "Unknown server error"
-            )
             self.set_status(status_code)
             self.write(
                 f"""
                 <h1>{status_code} - {title}</h1>
-                <p><strong>Error:</strong> {tornado.escape.xhtml_escape(error_msg)}</p>
                 """
             )
