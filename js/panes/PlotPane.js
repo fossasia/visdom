50e323412c906bae6b61463ad637c3035cc0aa1e
Saksham Sirohi <sakshamsirohi27@gmail.com>
2026-05-23 19:06:14 +0530

    Revert "plot panes now resize immediately upon dragging corner (#1228)"
    This reverts commit 7e35bed585627dba00f1406f724ca378b3b50c37.

--- js/panes/PlotPane.js
+++ js/panes/PlotPane.js
@@ -74,12 +74,6 @@ var PlotPane = (props) => {
     newPlot();
   });
 
-  useEffect(() => {
-    if (plotlyRef.current) {
-      Plotly.Plots.resize(plotlyRef.current);
-    }
-  }, [props.width, props.height]);
-  
   // rendering
   // ---------
 
