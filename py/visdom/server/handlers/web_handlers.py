d3886caeaeb8fdbd2a3731107d6cba8e06b70191
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-05-31 19:11:43 +0530

    Update web_handlers.py

--- py/visdom/server/handlers/web_handlers.py
+++ py/visdom/server/handlers/web_handlers.py
@@ -213,7 +213,7 @@ class UpdateHandler(BaseHandler):
                     if len(plot["z"]) != len(dz):
                         logging.error(
                             "ERROR: There is a mismatch between the number of rows in existing plot ('%i') and new data ('%i')."
-                            % (len(plot["z"]), len(dz))
+                            % (len(plot["z"][0]), len(dz)[0])
                         )
                         return p
                 updateNames = False
