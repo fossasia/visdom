5543d23cf2ddf0747c6960ce366ffcd6919ca31a
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-05-31 20:17:50 +0530

    Fix index error in logging for plot data mismatch

--- py/visdom/server/handlers/web_handlers.py
+++ py/visdom/server/handlers/web_handlers.py
@@ -205,7 +205,7 @@ class UpdateHandler(BaseHandler):
                     if len(plot["z"][0]) != len(dz[0]):
                         logging.error(
                             "ERROR: There is a mismatch between the number of columns in existing plot ('%i') and new data ('%i')."
-                            % (len(plot["z"][0]), len(dz)[0])
+                            % (len(plot["z"][0]), len(dz[0]))
                         )
                         return p
                 else:
