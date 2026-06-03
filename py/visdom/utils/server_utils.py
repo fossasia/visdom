7d13f89570508968778f5ead974f72dbb988e3a9
nightcityblade <nightcityblade@gmail.com>
2026-06-03 11:10:35 +0800

    fix: satisfy black 23 formatting

--- py/visdom/utils/server_utils.py
+++ py/visdom/utils/server_utils.py
@@ -396,9 +396,9 @@ def compare_envs(state, eids, socket, env_path=DEFAULT_ENV_PATH, show_all=False)
                     for dataIdx, data in enumerate(destWidJson["content"]["data"]):
                         if "name" not in data:
                             break  # stop working with this plot, not right format
-                        destWidJson["content"]["data"][dataIdx]["name"] = (
-                            "{}_{}".format(eidNums[eid], data["name"])
-                        )
+                        destWidJson["content"]["data"][dataIdx][
+                            "name"
+                        ] = "{}_{}".format(eidNums[eid], data["name"])
                 else:
                     if "name" not in destWidJson["content"]["data"][0]:
                         continue  # Skip windows with unnamed data
@@ -460,7 +460,9 @@ def compare_envs(state, eids, socket, env_path=DEFAULT_ENV_PATH, show_all=False)
         border: 1px solid black;
     }}
     </style>
-    <table> {} </table>""".format(" ".join(tableRows))
+    <table> {} </table>""".format(
+        " ".join(tableRows)
+    )
 
     res["jsons"]["window_compare_legend"] = {
         "command": "window",
