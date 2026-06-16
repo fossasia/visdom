c21091962bf5c21d10ff1f2a8974d54bf2c41cd4
JayantParashar10 <parasharjayant10@gmail.com>
2026-06-16 21:44:55 +0530

    revert: keep tornados comment unchanged in socket_handlers.py

--- py/visdom/server/handlers/socket_handlers.py
+++ py/visdom/server/handlers/socket_handlers.py
@@ -282,7 +282,7 @@ class VisSocketHandler(VisSocketHandlerOrWrapper):
 
 
 class VisSocketWrapper(VisSocketHandlerOrWrapper, AnySocketWrapper):
-    # this ignores tornado's initialization
+    # this ignores tornados initialization
     def __init__(self):
         self.polling = True
         pass
