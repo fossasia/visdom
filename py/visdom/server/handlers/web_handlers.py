1d3f3b38f14e39427a28a1d2ffd1549876884bf0
Debajeet Mandal <ninjarusher1411@gmail.com>
2026-06-19 19:29:20 +0530

    fix: require data only for content update windows

--- py/visdom/server/handlers/web_handlers.py
+++ py/visdom/server/handlers/web_handlers.py
@@ -119,7 +119,12 @@ class UpdateHandler(BaseHandler):
 
     @staticmethod
     def update(p, args, max_text_lines, max_old_content, max_image_history):
-        if "data" not in args:
+        if "data" not in args and p.get("type") in (
+            "text",
+            "embeddings",
+            "image_history",
+            "plot_history",
+        ):
             logging.warning("update() called without 'data' in args")
             return p
         # Update text in window, separated by a line break
