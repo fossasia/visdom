b26af9304e3816c9001e4804f4c360d99555ddcd
JayantParashar10 <parasharjayant10@gmail.com>
2026-06-19 13:49:22 +0530

    fix: add full dep array to TextPane useEffect and suppress no-console in EventSystem

--- js/panes/TextPane.js
+++ js/panes/TextPane.js
@@ -68,7 +68,7 @@ function TextPane(props) {
     return function cleanup() {
       EventSystem.unsubscribe('global.event', onEvent);
     };
-  }, [isFocused]);
+  }, [isFocused, sendPaneMessage, id, envID]);
 
   // rendering
   // ---------
