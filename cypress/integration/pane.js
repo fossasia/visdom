5dfd50a4ea5828a818b59d27997ffcf8e82ce8ee
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-06-21 01:10:01 +0530

    Update pane.js

--- cypress/integration/pane.js
+++ cypress/integration/pane.js
@@ -45,7 +45,7 @@ basic_examples.forEach( (setting) => {
 
       it('Drag & Drop Pane to 2nd Position', () => {
 
-          const targetpos = basic_example == "text_basic" ? 263 : basic_example == "image_basic" ? 276 : (basic_example == "misc_plot_matplot" || basic_example == "plot_special_graph") ? 10: basic_example == "misc_video_tensor" ? 263 : basic_example == "misc_audio_basic" ? 350 : basic_example == "properties_basic" ? 263 :390 : basic_example == "html_table" ? 263 : 390
+          const targetpos = basic_example == "text_basic" ? 263 : basic_example == "image_basic" ? 276 : basic_example == "misc_plot_matplot" || basic_example == "plot_special_graph" ? 10: basic_example == "misc_video_tensor" ? 263 : basic_example == "misc_audio_basic" ? 350 : basic_example == "properties_basic" ? 263 : basic_example == "html_table" ? 263 : 390
 
           cy
             .get('.layout .react-grid-item').first().should('have.css', 'transform', 'matrix(1, 0, 0, 1, 10, 10)')
