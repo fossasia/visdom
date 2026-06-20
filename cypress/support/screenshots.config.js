8a14495d803d9bc8c12fb6c8e6a3cd08ff078cc2
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-06-21 00:22:40 +0530

    Update screenshots.config.js

--- cypress/support/screenshots.config.js
+++ cypress/support/screenshots.config.js
@@ -57,7 +57,7 @@ export const all_screenshots = [
   'misc_arbitrary_visdom',
   'misc_getset_state',
   'properties_basic',
-  'misc_html_table',
+  'html_table',
 ];
 
 export const all_compareviews = [
