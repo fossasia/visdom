546851935d5cbec0513593da0553da8f134e5420
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-07-18 15:56:55 +0530

    refactor: drop unused env_path param from load_env
    load_env reads environments through the DataStore backend (store.load_env),
    so the env_path argument was never used in the function body. Remove it from
    the signature and from the socket handler call site.

--- py/visdom/utils/server_utils.py
+++ py/visdom/utils/server_utils.py
@@ -502,7 +502,7 @@ def send_to_sources(handler, msg):
         source.write_message(json.dumps(msg, cls=NanSafeEncoder))
 
 
-def load_env(state, eid, socket, store, env_path=DEFAULT_ENV_PATH):
+def load_env(state, eid, socket, store):
     """load an environment to a client by socket"""
     env = {}
     if eid in state:
