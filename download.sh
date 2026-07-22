50128d5fa9f09443560c7b8de85f41387c2b20ef
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-07-22 16:10:37 +0530

    Update download.sh

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ wget https://unpkg.com/jquery@3.1.1/dist/jquery.min.js -O py/static/js/jquery.mi
 wget https://unpkg.com/bootstrap@3.3.7/dist/js/bootstrap.min.js -O py/static/js/bootstrap.min.js
 wget https://unpkg.com/react@16.2.0/umd/react.production.min.js -O py/static/js/react-react.min.js
 wget https://unpkg.com/react-dom@16.2.0/umd/react-dom.production.min.js -O py/static/js/react-dom.min.js
-wget "https://cdnjs.cloudflare.com/ajax/libs/mathjax/4.0.0/tex-mml-chtml.js" -O py/static/js/mathjax-tex-mml-chtml.js
+wget "https://cdnjs.cloudflare.com/ajax/libs/mathjax/3.2.2/es5/tex-mml-svg.js" -O py/static/js/mathjax-tex-mml-svg.js
 wget https://cdn.rawgit.com/plotly/plotly.js/master/dist/plotly.min.js -O py/static/js/plotly-plotly.min.js
 wget https://unpkg.com/sjcl@1.0.7/sjcl.js -O py/static/js/sjcl.js
 wget https://cdnjs.cloudflare.com/ajax/libs/react-modal/3.6.1/react-modal.min.js -o py/static/js/react-modal.min.js
