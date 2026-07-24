bab6c4c65c7c959fdd8bf10f88bf5afcb8fa3eca
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-07-24 16:02:15 +0530

    Update PropertyItem.js

--- js/panes/PropertyItem.js
+++ js/panes/PropertyItem.js
@@ -8,6 +8,9 @@
  */
 
 import React, { useEffect, useRef, useState } from 'react';
+import { typesetMathJax } from './utils/mathjaxHelpers';
+
+const LATEX_PATTERN = /\$[^$]+\$|\\\([^)]+\\\)/;
 
 function EditablePropertyText(props) {
   const { value, validateHandler, submitHandler, blurStopPropagation } = props;
@@ -15,8 +18,12 @@ function EditablePropertyText(props) {
   // state varibles
   // --------------
   const textInput = useRef();
+  const previewRef = useRef();
   const [actualValue, setActualValue] = useState(value);
   const [isEdited, setIsEdited] = useState(false);
+
+  const hasLatex =
+    typeof actualValue === 'string' && LATEX_PATTERN.test(actualValue);
 
   // private events
   // --------------
@@ -42,6 +49,10 @@ function EditablePropertyText(props) {
     if (blurStopPropagation) event.stopPropagation();
   };
 
+  const handlePreviewClick = () => {
+    setIsEdited(true);
+  };
+
   // Enter invokes blur and thus submits the change
   const handleKeyPress = (event) => {
     if (event.key === 'Enter') textInput.current.blur();
@@ -55,8 +66,36 @@ function EditablePropertyText(props) {
     if (!isEdited) setActualValue(value);
   }, [value]);
 
+  useEffect(() => {
+    if (isEdited) textInput.current?.focus();
+  }, [isEdited]);
+
+  useEffect(() => {
+    if (isEdited || !hasLatex) return;
+    let cancelled = false;
+    typesetMathJax(previewRef.current, () => cancelled);
+    return () => {
+      cancelled = true;
+    };
+  }, [isEdited, hasLatex, actualValue]);
+
   // rendering
   // ---------
+
+  if (!isEdited && hasLatex) {
+    return (
+      <span
+        ref={previewRef}
+        role="textbox"
+        tabIndex={0}
+        style={{ cursor: 'text' }}
+        onClick={handlePreviewClick}
+        onFocus={handlePreviewClick}
+      >
+        {actualValue}
+      </span>
+    );
+  }
 
   return (
     <input
