481578f39afbe8e972893598a000c95fc09f2d13
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-08-01 23:12:42 +0530

    Update __init__.pyi

--- py/visdom/__init__.pyi
+++ py/visdom/__init__.pyi
@@ -336,7 +336,7 @@ class Visdom:
         env: _OptStr = ...,
         opts: _OptOps = ...,
     ) -> _SendReturn: ...
-    def table(
+    def html_table(
         self,
         headers: List[Any],
         data: List[List[Any]],
