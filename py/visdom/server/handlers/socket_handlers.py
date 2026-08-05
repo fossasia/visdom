24f1d3427df6cf820be8b6bb451eadabeb466583
Rajnish Tiwari <121179385+rajnisht7@users.noreply.github.com>
2026-08-06 01:17:05 +0530

    Update socket_handlers.py

--- py/visdom/server/handlers/socket_handlers.py
+++ py/visdom/server/handlers/socket_handlers.py
@@ -498,9 +498,10 @@ class AnySocketHandlerOrWrapper(BaseWebSocketHandler):
                 "version": p["version"],
             }
             broadcast(self, json.dumps(broadcast_packet, cls=NanSafeEncoder), eid)
+            state_snapshot = copy.deepcopy(self.state[eid])
 
             tornado.ioloop.IOLoop.current().run_in_executor(
-                None, self.storage.save_env, eid, self.state[eid]
+                None, self.storage.save_env, eid, state_snapshot
             )
             send_to_sources(
                 self,
