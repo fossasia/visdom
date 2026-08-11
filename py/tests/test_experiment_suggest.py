ed4c361b963fd26de6a92a430e4bc7ec9bb49874
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-08-11 14:13:03 +0530

    test: add the license header to the experiments test files

--- py/tests/test_experiment_suggest.py
+++ py/tests/test_experiment_suggest.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+
+# Copyright 2017-present, The Visdom Authors
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 """Tests for the experiment suggest stub (Layer 2, PR 6).
 
 ``/experiments/suggest`` is a reserved endpoint: the suggestion strategy
