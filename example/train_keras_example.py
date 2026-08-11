287faaebca2973b6b649d5579364e53d88b4281d
JayantParashar10 <parasharjayant10@gmail.com>
2026-08-11 17:48:39 +0530

    added header

--- example/train_keras_example.py
+++ example/train_keras_example.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+# Copyright 2017-present, The Visdom Authors
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import numpy as np
 from tensorflow import keras
 from tensorflow.keras import layers
