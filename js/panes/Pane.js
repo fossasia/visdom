2d1c9fbd60eebbaf8206cad6f4135e01d39416c8
Manik-Khajuria-5 <manik.khajuria491@gmail.com>
2026-08-20 14:30:21 +0530

    chore(hparams): drop the unused per-type pane classnames and keep the styling in CSS

--- js/panes/Pane.js
+++ js/panes/Pane.js
@@ -85,14 +85,8 @@ var Pane = forwardRef((props, ref) => {
 
   // rendering
   // ---------
-  let windowClassNames = classNames(
-    { window: true, focus: props.isFocused },
-    props.type && 'window-' + props.type
-  );
-  let barClassNames = classNames(
-    { bar: true, focus: props.isFocused },
-    props.type && 'bar-' + props.type
-  );
+  let windowClassNames = classNames({ window: true, focus: props.isFocused });
+  let barClassNames = classNames({ bar: true, focus: props.isFocused });
 
   let contentClassNames = classNames({
     content: true,
